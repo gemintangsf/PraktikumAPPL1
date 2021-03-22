b472ec2d1b29c11e36799a84e0f60fac08edd128
gemintangsf <gemintangsfurqon@gmail.com>
2021-03-22 20:49:50 +0700

    commitke2

--- Week2/ExceptionsAren'tAlwaysErrors/src/exceptionsaren/talwayserrors/CountLetters.java
+++ Week2/ExceptionsAren'tAlwaysErrors/src/exceptionsaren/talwayserrors/CountLetters.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exceptionsaren.talwayserrors;
-
 /**
  *
- * @author asus
+ * @author GemintangSangkajiFurqon
  */
 // ****************************************************************
 // CountLetters.java
@@ -17,9 +11,7 @@ package exceptionsaren.talwayserrors;
 //
 // ****************************************************************
 import java.util.Scanner;
-
 public class CountLetters {
-
     public static void main(String[] args) {
         int[] counts = new int[26];
         Scanner scan = new Scanner(System.in);
@@ -28,8 +20,6 @@ public class CountLetters {
         String word = scan.nextLine();
         //convert to all upper case
         word = word.toUpperCase();
-        
-
             //count frequency of each letter in string
         for (int i = 0; i < word.length(); i++) {
             try {
@@ -37,11 +27,9 @@ public class CountLetters {
             }
              catch (ArrayIndexOutOfBoundsException ex){
                  System.err.println("Not a letter \"" + word.charAt(i) + "\"");
-        }
-        
+        } 
         }
             //print frequencies
-
             for (int i = 0; i < counts.length; i++) {
                 if (counts[i] != 0) {
                     System.out.println((char) (i + 'A') + ": " + counts[i]);
---
+++ Week2/ThrowingExceptions/src/throwingexceptions/MathUtils.java
@@ -0,0 +1,25 @@
+package throwingexceptions;
+
+/**
+ *
+ * @author GemintangSangkajiFurqon
+ */
+// ****************************************************************
+// MathUtils.java
+//
+// Provides static mathematical utility functions.
+//
+// ****************************************************************
+public class MathUtils {
+    //-------------------------------------------------------------
+    // Returns the factorial of the argument given
+    //-------------------------------------------------------------
+
+    public static int factorial(int n) {
+        int fac = 1;
+        for (int i = n; i > 0; i--) {
+            fac *= i;
+        }
+        return fac;
+    }
+}
