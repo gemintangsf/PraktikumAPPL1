855440d305b86f3b5727fe066c4cbf00b8b073ff
gemintangsf <gemintangsfurqon@gmail.com>
2021-03-26 16:40:26 +0700

    kedua

--- Week2/ThrowingExceptions/src/throwingexceptions/Factorials.java
+++ Week2/ThrowingExceptions/src/throwingexceptions/Factorials.java
@@ -11,28 +11,23 @@ package throwingexceptions;
 //
 // ****************************************************************
 import java.util.Scanner;
-
 public class Factorials {
-
     public static void main(String[] args) {
         String keepGoing = "y";
         Scanner scan = new Scanner(System.in);
-        while (keepGoing.equals("y") || keepGoing.equals("Y"))
-        {
+        while (keepGoing.equals("y") || keepGoing.equals("Y")) {
             System.out.print("Enter an integer: ");
             int val = scan.nextInt();
-            try
-            {    
-                System.out.println("Factorial(" + val + ") = " 
-                    + MathUtils.factorial(val));  
+            try {
+
+                System.out.println("Factorial(" + val + ") = "
+                        + MathUtils.factorial(val));
+
+            } catch (IllegalArgumentException ex) {
+                System.err.println(ex.getMessage());
             }
-            catch(IllegalArgumentException ex)
-            {        
-                
-                System.out.print("Another factorial? (y/n) ");
-                keepGoing = scan.next();
-        
-            }
+            System.out.print("Another factorial? (y/n) ");
+            keepGoing = scan.next();
         }
     }
 }
--- Week2/ThrowingExceptions/src/throwingexceptions/MathUtils.java
+++ Week2/ThrowingExceptions/src/throwingexceptions/MathUtils.java
@@ -15,8 +15,14 @@ public class MathUtils {
     // Returns the factorial of the argument given
     //-------------------------------------------------------------
 
-    public static int factorial(int n) {
-        int fac = 1;
+    public static int factorial(int n) throws IllegalArgumentException {
+        int fac = 1;        
+        if ( n < 0 ){
+            throw new IllegalArgumentException("Factorial can't be negative");
+        }
+        else if ( n > 16 ){
+            throw new IllegalArgumentException("Overflow");
+        }                    
         for (int i = n; i > 0; i--) {
             fac *= i;
         }
