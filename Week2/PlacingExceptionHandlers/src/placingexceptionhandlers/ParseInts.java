594f83d66c6c3864b4a316e6746a1da8d453ecf7
gemintangsf <gemintangsfurqon@gmail.com>
2021-03-26 16:56:04 +0700

    kesatu

---
+++ Week2/PlacingExceptionHandlers/src/placingexceptionhandlers/ParseInts.java
@@ -0,0 +1,39 @@
+package placingexceptionhandlers;
+
+/**
+ *
+ * @author GemintangSangkajiFurqon
+ */
+// ****************************************************************
+// ParseInts.java
+//
+// Reads a line of text and prints the integers in the line.
+//
+// ****************************************************************
+import java.util.Scanner;
+
+public class ParseInts {
+
+    public static void main(String[] args) {
+        int val, sum = 0;
+        Scanner scan = new Scanner(System.in);
+        String line;
+        System.out.println("Enter a line of text");
+        Scanner scanLine = new Scanner(scan.nextLine());
+        
+        while (scanLine.hasNext()) {
+            
+            try
+            {
+                val = Integer.parseInt(scanLine.next());
+                sum += val; 
+            }
+            catch (NumberFormatException ex)
+            {
+                
+            }        
+            
+        }
+        System.out.println("The sum of the integers on this line is " + sum);
+    }
+}
