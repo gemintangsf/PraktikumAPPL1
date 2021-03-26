b58e6538d189f7ffeee0adf29749be694dfa5f0a
gemintangsf <gemintangsfurqon@gmail.com>
2021-03-26 17:42:07 +0700

    kedua

--- Week2/CopyingAFile/src/copyingafile/CopyingAFile.java
+++ Week2/CopyingAFile/src/copyingafile/CopyingAFile.java
@@ -10,7 +10,10 @@ import java.util.Scanner;
 public class CopyingAFile {
     public static void main(String[] args) {
         try {
+            //myObj untuk menyimpan data dari teks.txt
             File myObj = new File("teks.txt");
+            
+            //untuk mengambil data dan menampilkannya pada output
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
@@ -24,4 +27,3 @@ public class CopyingAFile {
         }
     }
 }
-
